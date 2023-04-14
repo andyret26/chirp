cf4abc4b72cf2904bb7948922b2a8e68b779bc9b
Anders <60463942+andyret26@users.noreply.github.com>
2023-04-14 13:01:17 +0200

    Added rate limiter

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -5,6 +5,23 @@ import {
 } from "@/server/api/trpc";
 import { z } from "zod";
 
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import { TRPCError } from "@trpc/server";
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
+
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => ctx.prisma.post.findMany({
     include: {
@@ -21,6 +38,10 @@ export const postsRouter = createTRPCRouter({
         content: z.string().emoji().min(1).max(255),
       }),
     ).mutation(async ({ ctx, input }) => {
+      const { success } = await ratelimit.limit(ctx.session.user.id);
+
+      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+
       const post = await ctx.prisma.post.create({
         data: {
           userId: ctx.session.user.id,
